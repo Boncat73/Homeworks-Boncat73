351d13e184bf2144a8ffc92259e5919bda93285a
Boncat73 <bonvas2305#urk.net>
2025-12-29 13:06:48 +0200

    Upgrade folder educational_project_1

--- educational_project_1/hello.py
+++ educational_project_1/hello.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
-@app.route("/")
+# Додаємо маршрут для головної сторінки
+@app.route('/')
 def index():
-    return "<h1>Заголовок h1</h1>"
+    return render_template('login.html')
+
+@app.route('/success/<name>')
+def success(name):
+    return 'Welcome , %s!' % name
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success',name = user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success',name = user))
+    
+
+# @app.route("/")
+# def index():
+#     return "<h1>Заголовок h1</h1>"
 
 # @app.route("/contacts")
 # def contacts():
@@ -23,7 +42,7 @@ def index():
 #     return "<h1>About Us</h1>"
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+    app.run(port=5000, debug=True)
 
 # from markupsafe import escape
 
