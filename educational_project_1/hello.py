fa0441ffc01fb9ba067f593b8b3728ba81c9255e
Boncat73 <bonvas2305#urk.net>
2025-12-29 11:53:14 +0200

    Upgrade edycational_project_1

--- educational_project_1/hello.py
+++ educational_project_1/hello.py
@@ -1,17 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-# приклад як на один і той же обробник "вішати" 
-# кілька URL:
-@app.route("/index")
 @app.route("/")
 def index():
     return "index"
 
+@app.route("/contacts")
+def contacts():
+    return "contacts"
+
 @app.route("/about")
 def about():
-    return "<h1>About Us</h1>"
+    return "about"
+
+# приклад як на один і той же обробник "вішати" 
+# кілька URL:
+# @app.route("/index")
+
+# @app.route("/about")
+# def about():
+#     return "<h1>About Us</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
