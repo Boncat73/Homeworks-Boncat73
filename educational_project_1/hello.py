ad28d27f11d11466a023fbd8a62091b8678848ae
Boncat73 <bonvas2305#urk.net>
2026-01-01 04:59:17 +0200

    Upgrade exucational_project_1

--- educational_project_1/hello.py
+++ educational_project_1/hello.py
@@ -24,67 +24,17 @@ def index():
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route("/contacts")
+def contacts():
+    return "contacts"
 
-# @app.route('/')
-# def index():
-#     return render_template('login.html')
+@app.route("/about")
+def about():
+    return "about"
 
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     if request.method == 'POST':
-#         user = request.form.get('nm', '').strip() # Отримуємо ім'я та видаляємо пробіли
-        
-#         if not user: # Перевірка: якщо рядок порожній
-#             flash("Будь ласка, введіть ваше ім'я!")
-#             return redirect(url_for('index'))
-            
-#         return redirect(url_for('success', name=user))
-#     return redirect(url_for('index'))
-
-# @app.route('/success/<name>')
-# def success(name):
-#     return render_template('welcome.html', user_name=name)
-    
-
-
-
-
-
-
-# @app.route("/")
-# def index():
-#     return "<h1>Заголовок h1</h1>"
-
-# @app.route("/contacts")
-# def contacts():
-#     return "contacts"
-
-# @app.route("/about")
-# def about():
-#     return "about"
-
-# приклад як на один і той же обробник "вішати" 
-# кілька URL:
-# @app.route("/index")
-
-# @app.route("/about")
-# def about():
-#     return "<h1>About Us</h1>"
-
-
-
-# from markupsafe import escape
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-# @app.route("/hello")
-# def hello():
-#     name = request.args.get("name", "Flask")
-#     return f"Hello, {escape(name)}!"
+@app.route("/welcome")
+def welcome():
+    return "welcome"
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
